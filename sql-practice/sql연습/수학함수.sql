ae22456c51a65cb28bb7dda17a1e11e0ab62520a
pocpoc0202 <pocpoc0202@naver.com>
2021-10-01 14:19:20 +0900

    10-01 날짜함수 추가

--- sql-practice/sql연습/수학함수.sql
+++ sql-practice/sql연습/수학함수.sql
@@ -21,3 +21,12 @@ select round(1.598); -- 2
 select round(1.498), round(1.498, 1); -- 소수점 첫째 자리까지 반올림
 select round(1.498), round(1.498, 0); -- 실수자리 반올림
 
+-- pow(x, y), power(x, y) x의 y승
+select pow(2, 10) ,power(10, 2);
+
+-- sign(x)
+select sign(20), sign(-100), sign(0);
+
+-- greatest(x. y, ...), least(x, y, ....) : min, max 값 찾기
+select greatest(10, 40, 20, 30), least(10, 40, 20, 30);
+select greatest('b', 'A', 'C'), greatest('hello', 'hellp', 'hellq');
